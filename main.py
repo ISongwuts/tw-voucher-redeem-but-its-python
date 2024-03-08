ae55b6d7183ab074d088985a052b72d3a9c44de6
ISongwut <isongwut.me@gmail.com>
2024-03-08 22:38:08 +0700

    refactor: main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from TWTransaction import TWTransaction
 import json
 if __name__ == "__main__":
     t: TWTransaction = TWTransaction(
-        '0944271168', 
-        'https://gift.truemoney.com/campaign/voucher_detail?v=662fe462daf74b14b001fd65ad006161643'
+        'Your mobile phone.', 
+        'Your redeem link or code.'
     )
     resData = t.redeem()
     print(json.dumps(resData, indent=2))
